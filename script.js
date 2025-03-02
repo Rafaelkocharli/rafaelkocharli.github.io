3dabc121492a744b52a752d358d1bb45c7051c11
Rafael <152785150+Rafaelkocharli@users.noreply.github.com>
2025-03-02 15:54:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const aboutMe = [
+  'I am <span>Rafael Kocharli</span>, a <span>frontend developer</span>. I have experience in creating <span>pet projects</span>, and I love working with React. Currently, I am studying at MPEI (Moscow Power Engineering Institute) majoring in "Information Technology and Computer Systems."',
+  "I learned web development on my own, which has allowed me to enhance my skills and confidence in creating cool interactive interfaces. My projects reflect my passion for development and my commitment to continuous learning.",
+  "<span>I am always excited about new opportunities and open to collaboration.</span> If you have any questions or want to discuss a project, feel free to reach out!",
+];
+
+const p = document.getElementById("aboutMe").children;
+console.log(p);
+for (let i = 0; i < aboutMe.length; i++) {
+  p[i * 2].innerHTML = aboutMe[i];
+}
